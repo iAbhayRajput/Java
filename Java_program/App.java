c6dba275c4853cd7e696a66ea946d75b2a06ad27
iAbhayRajput <126077059+iAbhayRajput@users.noreply.github.com>
2024-04-12 23:10:09 +0530

    Created App.java

---
+++ Java_program/App.java
@@ -0,0 +1,31 @@
+// An Applet skeleton.
+import java.awt.*;
+import java.applet.*;
+/*
+<applet code="App" width=300 height=100>
+</applet>
+*/
+public class App extends Applet {
+// Called first.
+public void init() {
+// initialization
+}
+/* Called second, after init(). Also called whenever
+the applet is restarted. */
+public void start() {
+// start or resume execution
+}
+// Called when the applet is stopped.
+public void stop() {
+// suspends execution
+}
+/* Called when applet is terminated. This is the last
+method executed. */
+public void destroy() {
+// perform shutdown activities
+}
+// Called when an applet’s window must be restored.
+public void paint(Graphics g) {
+// redisplay contents of window
+}
+}
